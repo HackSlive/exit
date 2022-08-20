a4a12d1eea25befd438a13dfe3c09c0f3164b77d
HackSlive <109677408+HackSlive@users.noreply.github.com>
2022-08-20 19:25:06 +0500

    Create exit auto-farm.lua

---
+++ exit auto-farm.lua
@@ -0,0 +1,5 @@
+while wait() do
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-900, -900, -900.662964, -900, -900, -900, -900, -900.997067571, -900.0703041703, -900.999481142, -900.0293653701, -900.0132355969)
+
+end
